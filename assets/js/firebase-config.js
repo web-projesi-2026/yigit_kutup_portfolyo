84c324286e892a16fb894d5bdb7338ceae93822a
Hamza <hamzakahveci447@gmail.com>
2026-06-10 13:54:56 +0300

    Cache-busting eklendi, Firebase compat mode duzeltildi

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase Configuration (compat mode - no ES module imports needed)
+// Firebase compat SDK is loaded via <script> tags in contact.html
+
 const firebaseConfig = {
   apiKey: "AIzaSyC11aVGnLfCu6WWPWlaE5XkQ4PQY2O-WLE",
   authDomain: "portfolyoy.firebaseapp.com",
@@ -14,6 +10,7 @@ const firebaseConfig = {
   appId: "1:47231265300:web:e668d1083a30547e001501",
   measurementId: "G-B8QT0TM260"
 };
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
